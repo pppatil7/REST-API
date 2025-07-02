09863fee3fbbe9d9eb0999129367f06d5456d9d5
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-02 17:37:52 +0530

    updated entity

--- src/main/java/com/springrest/springrest/entities/Course.java
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -7,4 +7,47 @@ public class Course {
     private String courseTitle;
 
     private String courseDescription;
+
+    public Course(long courseId, String courseTitle, String courseDescription) {
+        this.courseId = courseId;
+        this.courseTitle = courseTitle;
+        this.courseDescription = courseDescription;
+    }
+
+    public Course() {
+    }
+
+    public long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseTitle='" + courseTitle + '\'' +
+                ", courseDescription='" + courseDescription + '\'' +
+                '}';
+    }
+
 }
